c582ce329d04c41ee312e1582e63d49427b3b2eb
killerspark <hengallego@outlook.com>
2024-04-09 14:13:19 -0700

    api_key.py update
    updated API_KEY file and adds more information for the user to know about API keys.

--- logic/api_keys.py
+++ logic/api_keys.py
@@ -1,4 +1,7 @@
 # api_keys
-OPENAI_API_KEY = "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"
-WEATHER_API_KEY = "44b98da1dc3107572bb07c261060f469"
+OPENAI_API_KEY = "[insert openai api key here]"
+WEATHER_API_KEY = "[insert API key here from ]"
 JOKES_API_KEY = ""
+""" 
+If you don't have an openai api key, please type "notts" when launching hal. This will disable TTS features and 'hopefully' not cause any bugs.
+""" 
